36049904004733d573aa086d345550eaa941b349
Marcel Ullrich <ullrich.marcel@web.de>
2020-09-17 22:16:25 +0200

    split settings file

---
+++ modules/cred.py
@@ -0,0 +1,21 @@
+import os
+from credPrivate import *
+
+def findPath(p):
+    if not os.path.exists(p):
+        return "."+p
+    else:
+        return p
+
+miroUrl="https://miro.com/app/board/"
+database="data.db"
+boardFile="boards.txt"
+
+gitRepo=findPath("/storage/material/")
+overviewPNG=gitRepo+"sheets/warmup/pool/overviewPNG.tex"
+sheetFolder="sheets/warmup/2019/"
+sheetPlaceholder="SHEET_ID"
+sheetTex=gitRepo+sheetFolder+f"warmup_{sheetPlaceholder}.tex"
+
+# codiUrl="https://dpad.cs.uni-saarland.de/"
+# no access
