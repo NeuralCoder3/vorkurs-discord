f776a37d4506062d76124a77f75836a3fe313b1c
Marcel Ullrich <ullrich.marcel@web.de>
2020-09-20 11:35:42 +0200

    reorder

--- modules/cred.py
+++ modules/cred.py
@@ -6,19 +6,42 @@ except ImportError:
 
 def findPath(p):
     if not os.path.exists(p):
-        return "."+p
-    else:
-        return p
+        p="."+p
+        if not os.path.exists(p):
+            print(f"We have a problem: {p} does not exists.")
+    return p
+
+# URLs
 
 miroUrl="https://miro.com/app/board/"
+# codiUrl="https://dpad.cs.uni-saarland.de/"
+# no access
+
+# Files and placeholders
+
 database="data.db"
 boardFile="boards.txt"
-
-gitRepo=findPath("/storage/material/")
+aliasFile="defaultAlias.txt"
+gitRepo=findPath("/storage/materials/")
 overviewPNG=gitRepo+"sheets/warmup/pool/overviewPNG.tex"
 sheetFolder="sheets/warmup/2019/"
 sheetPlaceholder="SHEET_ID"
 sheetTex=gitRepo+sheetFolder+f"warmup_{sheetPlaceholder}.tex"
 
-# codiUrl="https://dpad.cs.uni-saarland.de/"
-# no access
+
+# Data
+
+warmupTime=(9,50)
+scheduleTime=(9,55)
+
+datemap=[
+    "A1","A2","A3","A4","A5","","",
+    "B1","B2","B3","B4","B5","","",
+    "C1","C2","C3","C4","C5","","",
+    "D1","D2","D3","D4","D5","","",
+    "","","","","",
+    "","","","","",
+    "","","","","",
+    "","","","","",
+    "","","","",""
+]
